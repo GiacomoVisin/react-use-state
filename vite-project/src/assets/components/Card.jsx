813506522af82127305531d1fe55dacfcfe05087
GiacomoVisin <killerboss1150@gmail.com>
2026-04-29 16:11:42 +0200

    fixing buttons

--- vite-project/src/assets/components/Card.jsx
+++ vite-project/src/assets/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({ item }) {
     return (
         <div className="card">
             <button onClick={() => setIsVisible(!isVisible)}
-                style={{ backgroundColor: isVisible ? "rgb(255, 193, 7)" : "rgb(13, 110, 253)" }}>  {item.title} </button>
+                style={{ backgroundColor: isVisible ? "rgb(255, 193, 7)" : "rgb(13, 110, 253)" , color: isVisible ? "black" : "white"}}>  {item.title} </button>
 
             {isVisible &&
                 <p>
