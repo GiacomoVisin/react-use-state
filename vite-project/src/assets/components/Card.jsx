d70d0766ce0e35425b97eb88002c947fb0422594
GiacomoVisin <killerboss1150@gmail.com>
2026-04-29 15:54:38 +0200

    first task complete, card and useState Working

--- vite-project/src/assets/components/Card.jsx
+++ vite-project/src/assets/components/Card.jsx
@@ -1,12 +1,20 @@
-export default function Card({item}) {
+import { useState } from "react"
 
+export default function Card({ item }) {
+    const [isVisible, setIsVisible] = useState(false)
 
     return (
         <div className="card">
-            <button> {item.title} </button>
-            <p>
-                {item.description}
-            </p>
+            <button onClick={() => setIsVisible(!isVisible)}
+                style={{ backgroundColor: isVisible ? "rgb(255, 193, 7)" : "rgb(13, 110, 253)" }}>  {item.title} </button>
+
+            {isVisible &&
+                <p>
+                    {item.description}
+                </p>
+
+            }
+
         </div>
     )
 }
--- vite-project/src/assets/components/CardContainer.jsx
+++ vite-project/src/assets/components/CardContainer.jsx
@@ -1,9 +1,9 @@
-import { use, useState } from "react"
+
 import Card from "./Card"
 import languages from "../../data/languages"
 
 export default function CardContainer({item}) {
-const [IsOn,SetIsOn] = useState (false)
+
 
     return (
         <>
