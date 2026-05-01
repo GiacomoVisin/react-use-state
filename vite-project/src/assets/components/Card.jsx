fea0481350f15d12f2c8335eab669b94c76b5be3
GiacomoVisin <killerboss1150@gmail.com>
2026-05-01 12:35:16 +0200

    fixing card

--- vite-project/src/assets/components/Card.jsx
+++ vite-project/src/assets/components/Card.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react"
 
 export default function Card({ item }) {
-    const [isVisible, setIsVisible] = useState(false)
+    
 
     return (
         <div className="card">
 
-
             <div>
                 {item.description}
             </div>
-
-
 
         </div>
     )
