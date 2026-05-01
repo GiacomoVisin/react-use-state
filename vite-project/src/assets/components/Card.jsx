63c7f88b552159563a5e893c26ec881a98ba10d8
GiacomoVisin <killerboss1150@gmail.com>
2026-05-01 11:16:25 +0200

    fixing the container card and the buttons

--- vite-project/src/assets/components/Card.jsx
+++ vite-project/src/assets/components/Card.jsx
@@ -5,13 +5,14 @@ export default function Card({ item }) {
 
     return (
         <div className="card">
-            <button onClick={() => setIsVisible(!isVisible)}
-                style={{ backgroundColor: isVisible ? "rgb(255, 193, 7)" : "rgb(13, 110, 253)" , color: isVisible ? "black" : "white"}}>  {item.title} </button>
-
+            <div>
+                <button onClick={() => setIsVisible(!isVisible)}
+                    style={{ backgroundColor: isVisible ? "rgb(255, 193, 7)" : "rgb(13, 110, 253)", color: isVisible ? "black" : "white" }}>  {item.title} </button>
+            </div>
             {isVisible &&
-                <p>
+                <div>
                     {item.description}
-                </p>
+                </div>
 
             }
 
--- vite-project/src/assets/components/CardContainer.jsx
+++ vite-project/src/assets/components/CardContainer.jsx
@@ -2,17 +2,29 @@
 import Card from "./Card"
 import languages from "../../data/languages"
 
-export default function CardContainer({item}) {
+export default function CardContainer({ item }) {
 
 
     return (
         <>
             <h1> Learn web development</h1>
-            <div className="ContainerCard">
-                {languages.map((item)=>(
-                      <Card key={item.id} item={item}/>
-                ))}
+            <div className="container">
+                <div className="ContainerCard">
+                    {languages.map((item) => (
+                        <Card key={item.id} item={item} />
+                    ))}
+                </div>
             </div>
         </>
+
+
+
+
+
+
+
+
+
+
     )
 }
